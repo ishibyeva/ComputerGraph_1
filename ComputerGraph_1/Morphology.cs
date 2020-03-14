d8d984d085b326527c5dc49776add030a36330aa
ishibyeva <43236369+ishibyeva@users.noreply.github.com>
2020-03-15 02:58:11 +0300

    Update Morphology.cs

--- ComputerGraph_1/Morphology.cs
+++ ComputerGraph_1/Morphology.cs
@@ -8,48 +8,49 @@ using System.ComponentModel;
 
 namespace ComputerGraph_1
 {
-    class Morphology: Filters
+
+    class Morphology : Filters
     {
-        protected int[,] st_elem;
+        protected float[,] st_elem;
         protected bool Dil;
         protected Morphology() //элемент - квадрат из единиц 
         {
-            st_elem = new int[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+            st_elem = new float[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
         }
-        public Morphology(int[,] st_elem)
+        public Morphology(float[,] st_elem_)
         {
-            this.st_elem = st_elem;
+            this.st_elem = st_elem_;
         }
 
         protected override Color calculateNewPixelColor(Bitmap Source, int W, int H)
         {
-            int Xdiam = st_elem.GetLength(0);//наш структурныый элемент 3х3, радиус по одной клетке
-            int Ydiam = st_elem.GetLength(1);
+            int radX = st_elem.GetLength(0)/2;//наш структурныый элемент 3х3, радиус по одной клетке
+            int radY = st_elem.GetLength(1)/2;
             int BITmaxR = 0; int BITmaxG = 0; int BITmaxB = 0; // черный
             int BITminR = 255; int BITminG = 255; int BITminB = 255;//белый
-            for (int j = -Ydiam / 2; j <= Ydiam / 2; j++)
-                for (int i = -Xdiam / 2; i <= Xdiam / 2; i++)
+            for (int j = -radY; j <= radY; j++)
+                for (int i = -radX; i <= radX; i++)
                 {
                     if (Dil) //если расширение
                     {
                         //дополняем все 1 в структурном элементе 
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).R > BITmaxR))
+                        if ((st_elem[i+radX, j+radY] == 1) && (Source.GetPixel(W + i, H + j).R > BITmaxR))
                             BITmaxR = Source.GetPixel(W + i, H + j).R;
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).G > BITmaxG))
+                        if ((st_elem[i + radX, j + radY] == 1) && (Source.GetPixel(W + i, H + j).G > BITmaxG))
                             BITmaxG = Source.GetPixel(W + i, H + j).G;
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).B > BITmaxB))
+                        if ((st_elem[i + radX, j + radY] == 1) && (Source.GetPixel(W + i, H + j).B > BITmaxB))
                             BITmaxB = Source.GetPixel(W + i, H + j).B;
 
                     }
                     else //сужение
                     {
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).R < BITminR))
+                        if ((st_elem[i + radX, j + radY] == 1) && (Source.GetPixel(W + i, H + j).R < BITminR))
 
                             BITminR = Source.GetPixel(W + i, H + j).R;
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).G < BITminG))
+                        if ((st_elem[i + radX, j + radY] == 1) && (Source.GetPixel(W + i, H + j).G < BITminG))
 
                             BITminG = Source.GetPixel(W + i, H + j).G;
-                        if ((st_elem[i, j] == 1) && (Source.GetPixel(W + i, H + j).B < BITminB))
+                        if ((st_elem[i + radX, j + radY] == 1) && (Source.GetPixel(W + i, H + j).B < BITminB))
 
                             BITminB = Source.GetPixel(W + i, H + j).B;
                     }
@@ -61,16 +62,16 @@ namespace ComputerGraph_1
         }
         public override Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
         {
-            int Xdiam = st_elem.GetLength(0);
-            int Ydiam = st_elem.GetLength(1);
+            int radX = st_elem.GetLength(0)/2;
+            int radY = st_elem.GetLength(1)/2;
             Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
-            for (int i = Xdiam / 2; i < sourceImage.Width - Xdiam / 2; i++)
+            for (int i = radX; i < sourceImage.Width - radX; i++)
             {
                 worker.ReportProgress((int)((float)i / resultImage.Width * 100));
                 //прерывание процесса 
                 if (worker.CancellationPending)
                     return null;
-                for (int j = Ydiam / 2; j < sourceImage.Height - Ydiam / 2; j++) // применяем эффект сверху вниз, потом в ширину 
+                for (int j = radY; j < sourceImage.Height - radY; j++) // применяем эффект сверху вниз, потом в ширину 
                     resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j));
             }
             return resultImage;
@@ -82,7 +83,7 @@ namespace ComputerGraph_1
         {
             Dil = true;
         }
-        public Dilation(int[,] st_elem_)
+        public Dilation(float[,] st_elem_)
         {
             this.st_elem = st_elem_;
             Dil = true;
@@ -94,7 +95,7 @@ namespace ComputerGraph_1
         {
             Dil = false;
         }
-        public Erosion(int[,] st_elem_)
+        public Erosion(float[,] st_elem_)
         {
             this.st_elem = st_elem_;
             Dil = false;
@@ -148,9 +149,10 @@ namespace ComputerGraph_1
         {
             Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
             Filters Err = new Erosion(st_elem);
+            Bitmap resultE = Err.processImage(sourceImage, worker);
+
             Filters Dila = new Dilation(st_elem);
             Bitmap resultD = Dila.processImage(sourceImage, worker);
-            Bitmap resultE = Err.processImage(sourceImage, worker);
             for (int i = 0; i < sourceImage.Width; i++)
             {
                 worker.ReportProgress((int)((float)i / resultImage.Width * 100));
@@ -164,7 +166,7 @@ namespace ComputerGraph_1
                     int newR = Clamp(resultD.GetPixel(i, j).R - resultE.GetPixel(i, j).R, 0, 255);
                     int newG = Clamp(resultD.GetPixel(i, j).G - resultE.GetPixel(i, j).G, 0, 255);
                     int newB = Clamp(resultD.GetPixel(i, j).B - resultE.GetPixel(i, j).B, 0, 255);
-                    resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j));
+                    resultImage.SetPixel(i, j, Color.FromArgb(newR,newG,newB));
                 }
             }
             return resultImage;
